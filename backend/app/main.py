73ee84618a3d08c9f7411100efd192c4b74b6661
ShamLucas <60859150+ShamLucas@users.noreply.github.com>
2026-01-27 11:04:46 +0100

    feat(backend: endpoint health and root ok

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
 
+# Créer l'instance de l'application
 app = FastAPI()
+
+# Défini une route GET sur "/"
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World!"}
+
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}
